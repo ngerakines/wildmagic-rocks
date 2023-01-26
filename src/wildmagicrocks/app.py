2c5873c506b466adf5ed1a1b3c640ad87fce5356
Nick Gerakines <nick.gerakines@gmail.com>
2023-01-26 14:39:08 -0500

    moving main around

--- src/wildmagicrocks/app.py
+++ src/wildmagicrocks/app.py
@@ -3,6 +3,9 @@ from aiohttp import web
 from wildmagicrocks.config import configure_logging
 from wildmagicrocks.server import start_web_server
 
-if __name__ == "__main__":
+def main():
     configure_logging()
     web.run_app(start_web_server())
+
+if __name__ == "__main__":
+    main()
